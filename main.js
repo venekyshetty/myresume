06aa590fbf2089cff66cbeced1131b44ea02f693
venkeyshetty <59465211+venkeyshetty@users.noreply.github.com>
2020-01-15 11:45:57 +0530

    Android files

---
+++ main.js
@@ -0,0 +1,29 @@
+alert("Hello world");
+
+
+console.log("HEllo Javascript")
+console.error("this is error");     
+console.warn("this is warning");
+/*let s="shiva";
+console.log(typeof(s));
+console.log(s);
+
+let g=100;
+console.log(typeof(g));
+let h=true;
+console.log(typeof(h));
+let j=null;
+console.log(typeof(j));
+let i=undefined;
+console.log(typeof(i));
+
+let z="venkey";
+let q="23"
+
+console.log("MY NAME IS $(Z)");*/
+
+ const s="hello world";
+// console.log(s.length);
+console.log(s.toUpperCase())
+document.getElementById('date').innerHTML= new Date()
+
